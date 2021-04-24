4948638c722fe881dc651acf1a85654ccb730920
Seh Wan Loong <sehwan1997@hotmail.com>
2021-04-24 13:09:13 +0800

    initial setup with react, sass, bootstrap

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDom from "react-dom"
+import App from "./App"
+// import "./App.css"
+import "bootstrap";
+import './style.scss';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDom.render(<App />, document.getElementById('app'))
